df5080293770297c9942aabf8751e36e01b70dc1
gilsondev <me@gilsondev.in>
2016-01-02 11:29:03 -0200

    Iniciando versionamento

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var methodOverride = require('method-override');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var error = require('./middlewares/error');
+
+var app = express();
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(cookieParser('ntalk'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+
+load('models')
+	.then('controllers')
+	.then('routes')
+	.into(app);
+
+app.use(error.notFound);
+app.use(error.serverError);
+
+io.sockets.on('connection', function(client) {
+	client.on('send-server', function(data) {
+		var msg = "<b>" + data.nome + ":</b>" + data.msg + "<br/>";
+		client.emit('send-client', msg);
+		client.broadcast.emit('send-client', msg);
+	});
+});
+
+server.listen(3000, function() {
+	console.log("Ntalk no ar");
+});
